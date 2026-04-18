a433f7514274c6fdd2e25a9be2f8988a2f495483
SailendraDarbha94 <sailendra.darbha@gmail.com>
2026-04-18 15:30:09 +0530

    env variables obscured

--- app/lib/firebaseConfig.ts
+++ app/lib/firebaseConfig.ts
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBseMSmc5uLWvIzErVqtztQTymviqjLe7c",
-  authDomain: "darbha-baburao.firebaseapp.com",
-  projectId: "darbha-baburao",
-  storageBucket: "darbha-baburao.firebasestorage.app",
-  messagingSenderId: "774224498636",
-  appId: "1:774224498636:web:d901e4b78dc83f5429d769"
+  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
